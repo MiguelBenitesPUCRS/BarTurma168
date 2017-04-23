c6428406bd11eb863ea249b76a9c2a4d18c29cc7
Fábio <Fbio@192.168.25.6>
2017-04-23 13:40:17 -0300

    Outros testes...

---
+++ src/tests/tAdicionaClienteTeste.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import business.Cliente;
+
+public class tAdicionaClienteTeste {
+
+	Cliente cliente;
+	
+	@Before
+	public void setUp()throws Exception{
+		cliente = new Cliente();
+	}
+	
+	@Test
+	public void testeNome(){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
